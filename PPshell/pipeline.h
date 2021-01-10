6d15cb4a7498243b3b3c9e86838e6c950c822d1d
Peter Fačko <pfacko1@gmail.com>
2021-01-10 11:57:32 +0100

    is_valid should take const ptr

--- PPshell/pipeline.h
+++ PPshell/pipeline.h
@@ -18,12 +18,12 @@ void pipeline_construct_move(pipeline_t* pipeline, pipeline_t* other);
 /** Special function. */
 void pipeline_destroy(pipeline_t* pipeline);
 /** Special function. */
-bool pipeline_is_valid(pipeline_t* pipeline);
+bool pipeline_is_valid(const pipeline_t* pipeline);
 
 /** Constructs a valid pipeline with a single 'command'. Moves 'command'. */
-void pipeline_construct_move_command(pipeline_t* pipeline, command_t* command);
+int pipeline_construct_move_command(pipeline_t* pipeline, command_t* command);
 /** Appends 'command' at the end of the 'pipeline'. Moves 'command'. */
-void pipeline_append_move_command(pipeline_t* pipeline, command_t* command);
+int pipeline_append_move_command(pipeline_t* pipeline, command_t* command);
 
 /** Executes the pipeline, storing the return value in 'return_value'. */
 int pipeline_execute(pipeline_t* pipeline, int* return_value);
