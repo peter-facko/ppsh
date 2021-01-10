f73d6cdea42e65e80c00931eab49c9ba4c73c9c5
Peter Fačko <pfacko1@gmail.com>
2021-01-10 11:55:24 +0100

    now also defines _get_head functions

--- PPshell/stailq_helpers.h
+++ PPshell/stailq_helpers.h
@@ -1,20 +1,35 @@
 #pragma once
 
-/** Creates a static function defintion which pops the first element from a
- * structure using STAILQ.
+/** Creates helper static function defintions for a structure that uses STAILQ.
  *
- * The 'list_type' needs to implement `list_type##_get_head` function, which
- * returns the head of the STAILQ.
+ * Creates:
+ * 		list_type##_get_head_const - gets the head of STAILQ
+ * 		list_type##_get_head - non-const version
+ * 		list_type##_pop_and_destroy - pops the first elements and destroys it
  *
  * The 'element_type' needs to implement `element_type##_destroy` special
  * function.
- *
- * The popped element is also destroyed.
  */
-#define CREATE_STAILQ_POP_AND_DESTROY(list_type, element_type, link_name)      \
+#define CREATE_STAILQ_HELPERS(list_type, head_type, head_name, element_type,   \
+							  link_name)                                       \
+	static const struct head_type* list_type##_get_head_const(                 \
+		const struct list_type* l)                                             \
+	{                                                                          \
+		assert(l != NULL);                                                     \
+                                                                               \
+		return &l->head_name;                                                  \
+	}                                                                          \
+	static struct head_type* list_type##_get_head(struct list_type* l)         \
+	{                                                                          \
+		assert(l != NULL);                                                     \
+                                                                               \
+		return (struct head_type*)list_type##_get_head_const(l);               \
+	}                                                                          \
 	static struct element_type* list_type##_pop_and_destroy(                   \
 		struct list_type* l)                                                   \
 	{                                                                          \
+		assert(l != NULL);                                                     \
+                                                                               \
 		struct element_type* e = STAILQ_FIRST(list_type##_get_head(l));        \
 		element_type##_destroy(e);                                             \
 		STAILQ_REMOVE(list_type##_get_head(l), e, element_type, link);         \
